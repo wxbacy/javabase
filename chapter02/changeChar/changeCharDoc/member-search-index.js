ac4424177ff6c8a926bdc69571c0b3fe0373dbda
chenwei <chenwei@jiancent.com>
2025-05-09 18:18:18 +0800

    删除.class

--- chapter02/changeChar/changeCharDoc/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"<Unnamed>","c":"ChangeChar","l":"ChangeChar()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"ChangeChar","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
